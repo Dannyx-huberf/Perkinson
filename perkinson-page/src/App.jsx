08ac0720f35c3b59bf17ee97bb8966fe98f6b46e
Daniel <maddisonjames891@gmail.com>
2025-09-30 15:03:28 +0100

    Initial Commit

---
+++ perkinson-page/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyButton from "./components/my-components/MyButton";
+
+const App = () => {
+  return (
+    <div>
+      <MyButton />
+    </div>
+  );
+};
+
+export default App;
---
+++ perkinson-page/src/components/my-components/MyButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../ui/button";
+const MyButton = () => {
+  return (
+    <div>
+      <Button className={`btn`}>Perkison</Button>
+      <h1 className="font-projectfontbold">Hii</h1>
+    </div>
+  );
+};
+
+export default MyButton;
