1145e6b9e7d12bfd00c8410b682e72a4a2051619
Daniel <maddisonjames891@gmail.com>
2025-09-30 21:23:08 +0100

    small touches

--- perkinson-page/src/components/my-components/Navigations.jsx
+++ perkinson-page/src/components/my-components/Navigations.jsx
@@ -3,18 +3,18 @@ import logo from "../../assets/image/perkinsonlogo.png";
 import { Navigation, Learn, courses, quicklinks } from "@/constants/utlitites";
 const Navigations = () => {
   return (
-    <section className="lg:flex lg:justify-between lg:gap-[155px]">
+    <section className="lg:flex lg:justify-between lg:gap-[140px]">
       <div className="">
         <img src={logo} alt="perkinson-logo" className="w-[267px]" />
       </div>
 
-      <div className="text-white  lg:flex lg:flex-1 lg:gap-[80px]">
+      <div className="text-white  lg:flex lg:flex-1 lg:gap-[65px]">
         <div>
           <h1 className="text-24px font-projectfont font-[900] mb-3">
             Quick Links
           </h1>
 
-          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px]  text-[17px]">
+          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px]  text-[16px]">
             {quicklinks.map((links, index) => (
               <li key={index} className="cursor-pointer link-hover">
                 {links}
@@ -27,7 +27,7 @@ const Navigations = () => {
           <h1 className="text-24px font-projectfont font-[900] mb-3">
             Navigation
           </h1>
-          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px] text-[17px]">
+          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px] text-[16px]">
             {Navigation.map((links, index) => (
               <li key={index} className="cursor-pointer link-hover">
                 {links}
@@ -38,7 +38,7 @@ const Navigations = () => {
 
         <div>
           <h1 className="text-24px font-projectfont font-[900] mb-3">Learn</h1>
-          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px] text-[17px]">
+          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px] text-[16px]">
             {Learn.map((links, index) => (
               <li key={index} className="cursor-pointer link-hover">
                 {links}
@@ -51,7 +51,7 @@ const Navigations = () => {
           <h1 className="text-24px font-projectfont font-[900] mb-3">
             Courses
           </h1>
-          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px] text-[17px]">
+          <ul className="font-projectfont2 text-textcolor font-[400] leading-[30px] text-[16px]">
             {courses.map((links, index) => (
               <li key={index} className="cursor-pointer link-hover">
                 {links}
