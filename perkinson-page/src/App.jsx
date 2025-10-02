2e8447b6f22b1ce3ac21f07bb8be23ce0020348a
Daniel <maddisonjames891@gmail.com>
2025-10-02 18:10:25 +0100

    small touches

--- perkinson-page/src/App.jsx
+++ perkinson-page/src/App.jsx
@@ -4,21 +4,23 @@ import NavBar from "./components/NavBar";
 import { HeroSection } from "./components/HeroSection";
 import Classes from "./pages/Classes";
 import Client from "./pages/Client";
+import HeroNav from "./components/nav-menu/HeroNav";
+import StackCard from "./components/nav-menu/StackCard";
 
 const App = () => {
   return (
     <div className="lg:w-full  mx-auto">
       <NavBar />
       <HeroSection />
-      <div className='flex flex-col items-center'>
-      <nav className='w-full flex justify-center py-6'>
-        <HeroNav />
-      </nav>
+      <div className="flex flex-col items-center">
+        <nav className="w-full flex justify-center py-6">
+          <HeroNav />
+        </nav>
 
-      <div className='w-full flex justify-center mt-4'>
-        <StackCard />
+        <div className="w-full flex justify-center mt-4">
+          <StackCard />
+        </div>
       </div>
-    </div>
       <Client />
       <Classes />
       <Faq />
--- perkinson-page/src/components/nav-menu/StackCard.jsx
+++ perkinson-page/src/components/nav-menu/StackCard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import stackCard from '../../assets/img/stack-card.png';
+import React from "react";
+import stackCard from "../../assets/img/stack-card.png";
 
 export default function StackCard() {
   return (
-    <div className='max-w-5xl w-full flex justify-center'>
-      <img src={stackCard} alt='Stack Card' className='rounded-xl shadow-lg' />
+    <div className="max-w-5xl w-full flex justify-center mb-15 lg:mb-10">
+      <img src={stackCard} alt="Stack Card" className="rounded-xl shadow-lg" />
     </div>
   );
 }
