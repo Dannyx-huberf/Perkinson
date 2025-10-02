92a876c8452639a8bc457ea792520c14ea1f14b8
Daniel <maddisonjames891@gmail.com>
2025-10-02 16:44:42 +0100

    nothing

--- perkinson-page/src/App.jsx
+++ perkinson-page/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import { HeroSection } from "./components/HeroSection";
 
 const App = () => {
   return (
     <div className="w-[90%] lg:w-[80%] mx-auto">
       <NavBar />
+      <HeroSection />
     </div>
   );
 };
